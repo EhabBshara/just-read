c8de9567740b77c610f6a5a1c6675453627fa915
Ehab Elbshara <ehabbshara1@hotmail.com>
2022-05-15 23:54:58 +1000

    add go to page in the drop down menu and a jankey scroll bar also change the default color to cyan for easier reading, colors will be customisable soon..

--- lib/screens/read.dart
+++ lib/screens/read.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:just_read/models/settings.dart';
 import 'package:just_read/widgets/appbar.dart';
 import 'package:provider/provider.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 class Reading extends StatelessWidget {
   final _pages = <dynamic>[];
@@ -36,24 +38,7 @@ class Reading extends StatelessWidget {
     if (snapshot.hasData) {
       // add pages as they arrive to the list to display.
       _pages.add(snapshot.data);
-      return ListView.separated(
-        separatorBuilder: (context, index) => Column(
-          children: [
-            Text(
-              (index + 1).toString(),
-              style: TextStyle(fontSize: 20, color: Colors.yellow),
-            ),
-            Divider(
-              color: Colors.yellow,
-              thickness: 2,
-            )
-          ],
-        ),
-        itemCount: _pages.length,
-        itemBuilder: (context, index) {
-          return PageView(page: _pages[index]);
-        },
-      );
+      return DocView(pages: _pages);
     } else if (snapshot.hasError) {
       children = <Widget>[
         const Icon(
@@ -89,10 +74,66 @@ class Reading extends StatelessWidget {
   }
 }
 
-class PageView extends StatelessWidget {
+class DocView extends StatefulWidget {
+  final List<dynamic> pages;
+  const DocView({Key? key, required this.pages}) : super(key: key);
+  @override
+  _DocViewState createState() => _DocViewState();
+}
+
+class _DocViewState extends State<DocView> {
+  ItemScrollController _scrollController = ItemScrollController();
+  final ItemPositionsListener _positionController =
+      ItemPositionsListener.create();
+
+  @override
+  Widget build(BuildContext context) {
+    int goToNum = context.read<Settings>().goToPageNum;
+    if (goToNum != 0) {
+      _scrollController.scrollTo(
+          index: goToNum - 1,
+          duration: Duration(microseconds: 200),
+          curve: Curves.easeInOutCubic);
+      context.read<Settings>().goToPage(0);
+    }
+    return Scrollbar(
+      interactive: true,
+      isAlwaysShown: true,
+      child: ScrollablePositionedList.separated(
+        itemScrollController: _scrollController,
+        itemPositionsListener: _positionController,
+        separatorBuilder: (context, index) => Column(
+          children: [
+            Text(
+              (index + 1).toString(),
+              style: TextStyle(fontSize: 20, color: Colors.yellow),
+            ),
+            Divider(
+              color: Colors.yellow,
+              thickness: 2,
+            )
+          ],
+        ),
+        itemCount: widget.pages.length,
+        itemBuilder: (context, index) {
+          int target;
+          if (goToNum == 0) {
+            target = index;
+          } else {
+            target = goToNum;
+            goToNum = 0;
+          }
+          return PageViews(page: widget.pages[target]);
+        },
+      ),
+    );
+  }
+}
+
+class PageViews extends StatelessWidget {
   final String page;
 
-  PageView({required this.page});
+  PageViews({required this.page});
 
   @override
   Widget build(BuildContext context) {
